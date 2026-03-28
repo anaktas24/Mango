bb43bb99edbee8803c95bb9054ac2d926ab92072
Ioannis Antoniou <johnantony24@hotmail.com>
2026-03-28 22:03:22 +0100

    Add Rails backend and ruby version

---
+++ backend/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      devise_for :users,
+        path: "",
+        path_names: {
+          sign_in: "users/sign_in",
+          sign_out: "users/sign_out",
+          registration: "users/sign_up"
+        },
+        controllers: {
+          sessions: "api/v1/users/sessions",
+          registrations: "api/v1/users/registrations"
+        }
+    end
+  end
+end
